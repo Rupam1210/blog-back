84cc1ce80b84180d7d9a6efc7987c3454981d428
Rupam1210 <145320098+Rupam1210@users.noreply.github.com>
2025-03-23 21:35:46 +0530

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -24,25 +24,47 @@ const path=useLocation().pathname
     setmenu(!menu)
   }
   return (
-    <div className="nav">
-     <h1 className='title'><Link to={"/"}>Blog Master</Link></h1>
+    // <div className="nav">
+    //  <h1 className='title'><Link to={"/"}>Blog Master</Link></h1>
+    // {path==="/" &&  <div className="search">
+    //   <p onClick={()=>navigate(prompt?"?search="+prompt: "/")} ><BsSearch/></p>
+    //   <input onChange={(e)=>setprompt(e.target.value)} type="text" placeholder='Search a Post' />
+    //  </div>}
+    //  <div className="box">
+    //       {user?(<Link to={"/write"}>Write</Link>): (<Link to={"/login"}>Login</Link> )}
+    //       {user?(<div onClick={handleclick} style={{cursor:"pointer",position:"relative" ,color:"white",fontSize:"20px"}}>
+    //            <p><FaBars/></p>
+    //            {menu && <Menu/>}
+    //           </div>
+    //       ):(<Link to={"/register"}>Register</Link>)}
+    //  </div>
+    //  <div onClick={handleclick} className="menu">
+    //   <p><FaBars/></p>
+    //   {menu && <Menu/>}
+    //  </div>
+
+    // </div>
+       <div className="navbar">
+     <h1 className='title' style={{color:"white"}}><Link to={"/"} style={{color:"white"}}>  ThoughtGrid</Link></h1>
     {path==="/" &&  <div className="search">
+      
+      <input onChange={(e)=>setprompt(e.target.value)} type="text" placeholder='Search a Post' className='search-box' />
       <p onClick={()=>navigate(prompt?"?search="+prompt: "/")} ><BsSearch/></p>
-      <input onChange={(e)=>setprompt(e.target.value)} type="text" placeholder='Search a Post' />
      </div>}
-     <div className="box">
-          {user?(<Link to={"/write"}>Write</Link>): (<Link to={"/login"}>Login</Link> )}
+     <div className="box" style={{display:"flex",gap:"15px",alignItems:"center"}}>
+          {user?(<Link to={"/write"} className='write-btn'>Write</Link>): (<Link to={"/login"}>Login</Link> )}
           {user?(<div onClick={handleclick} style={{cursor:"pointer",position:"relative" ,color:"white",fontSize:"20px"}}>
-               <p><FaBars/></p>
-               {menu && <Menu/>}
-              </div>
-          ):(<Link to={"/register"}>Register</Link>)}
-     </div>
-     <div onClick={handleclick} className="menu">
+            
+               {/* {menu && <Menu/>} */}
+               <div onClick={handleclick} className="menu">
       <p><FaBars/></p>
       {menu && <Menu/>}
      </div>
 
+              </div>
+          ):(<Link to={"/register"}>Register</Link>)}
+     </div>
+     
     </div>
   )
 }
